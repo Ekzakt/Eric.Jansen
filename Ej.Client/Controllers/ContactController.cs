7466dc0c0eec6dbc48f5ac77d6d1e10147997765
EricJansen <EricJansen@LAPTOP-EJ>
2024-07-23 00:05:13 -0400

    Added English content to resource files.

--- Ej.Client/Controllers/ContactController.cs
+++ Ej.Client/Controllers/ContactController.cs
@@ -1,13 +1,13 @@
 ﻿using Ej.Application.Configuration;
 using Ej.Application.Contracts;
 using Ej.Application.Models;
-using Ej.Client.Extensions;
 using Ej.Infrastructure.Configuration;
 using Ej.Infrastructure.Queueing;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
+using Ekzakt.Utilities;
 
 namespace Ej.Client.Controllers;
 
@@ -17,24 +17,37 @@ public class ContactController : BaseController
     private readonly EricJansenOptions _options;
     private readonly IQueueService _queueService;
     private readonly ITenantProvider _tenantProvider;
+    private readonly IHtmlLocalizer<ContactController> _localizer;
 
     public ContactController(
         IOptions<CultureOptions> cultureOptions,
         IValidator<ContactViewModel> validator,
         IOptions<EricJansenOptions> options,
         IQueueService queueService,
-        ITenantProvider tenantProvider) : base(cultureOptions)
+        ITenantProvider tenantProvider,
+        IHtmlLocalizer<ContactController> localizer) : base(cultureOptions)
     {
         _options = options?.Value ?? throw new ArgumentNullException(nameof(options));
         _validator = validator ?? throw new ArgumentNullException(nameof(validator));
         _queueService = queueService ?? throw new ArgumentNullException(nameof(queueService));
         _tenantProvider = tenantProvider ?? throw new ArgumentNullException(nameof(tenantProvider));
+        _localizer = localizer ?? throw new ArgumentNullException(nameof(localizer));
     }
 
 
     [Route("{culture:culture}/contact")]
     public IActionResult Index()
     {
+        ViewData["Title"] = _localizer["__View_Index_Title"].Value;
+        ViewData["MetaTitle"] = _localizer["__View_Index_MetaTitle"].Value;
+
+        ViewBag.PageTitle = _localizer["__View_Index_Title"].Value;
+        ViewBag.Content = _localizer["__View_Index_Content"].Value;
+        ViewBag.FormName = _localizer["__View_Index_Form_Name"].Value;
+        ViewBag.FormEmail = _localizer["__View_Index_Form_Email"].Value;
+        ViewBag.FormMessage = _localizer["__View_Index_Form_Message"].Value;
+        ViewBag.FormSubmit = _localizer["__View_Index_Form_Submit"].Value;
+
         return View();
     }
 
@@ -43,6 +56,7 @@ public class ContactController : BaseController
     [Route("{culture:culture}/contact")]
     public async Task<ActionResult> Post(ContactViewModel model)
     {
+        StringReplacer stringReplacer = new();
         ValidationResult result = await _validator.ValidateAsync(model);
 
         if (!result.IsValid)
@@ -64,15 +78,44 @@ public class ContactController : BaseController
 
             if (await _queueService.SendMessageAsync(_options?.QueueNames?.ContactForm ?? string.Empty, message))
             {
+                ViewData["Title"] = _localizer["__View_Success_Title"].Value;
+                ViewData["MetaTitle"] = _localizer["__View_Success_MetaTitle"];
+
+                ViewBag.PageTitle = ReplaceTitleContent(stringReplacer, _localizer["__View_Success_PageTitle"].Value, model.Name);
+                ViewBag.Content = ReplacePageContent(stringReplacer, _localizer["__View_Success_Content"].Value, _tenantProvider.Tenant!);
+
                 return View("Success", model);
             }
 
+            ViewData["Title"] = _localizer["__View_Error_Title"].Value;
+            ViewData["MetaTitle"] = _localizer["__View_Error_MetaTitle"];
+
+            ViewBag.PageTitle = ReplaceTitleContent(stringReplacer, _localizer["__View_Error_PageTitle"].Value, model.Name);
+            ViewBag.Content = ReplacePageContent(stringReplacer, _localizer["__View_Error_Content"].Value, _tenantProvider.Tenant!);
+
             return View("Error", model);
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            ViewBag.Message = "Woops, I fucked up this time.  Something went seriously wrong." + ex.Message;
             throw;
         }
     }
+
+
+    #region Helpers
+
+    private string ReplaceTitleContent(StringReplacer stringReplacer, string title, string replacement)
+    {
+        stringReplacer.AddReplacement("Contact_Name", replacement);
+
+        return stringReplacer.Replace(title);
+    }
+
+
+    private string ReplacePageContent(StringReplacer stringReplacer, string content, Tenant tenant)
+    {
+        return stringReplacer.ReplaceTenantProperties(tenant, content);
+    }
+
+    #endregion Helpers
 }
---
+++ Ej.Client/Extensions/StringReplacerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Ej.Application.Models;
+using Ekzakt.Utilities;
+
+namespace Ej.Client.Extensions;
+
+public static class StringReplacerExtensions
+{
+    public static string ReplaceTenantProperties(this StringReplacer stringReplacer, Tenant tenant, string content)
+    {
+        stringReplacer.AddReplacement("Tenant_Name", tenant?.Name ?? string.Empty);
+        stringReplacer.AddReplacement("Tenant_ShortName", tenant?.ShortName ?? string.Empty);
+
+        return stringReplacer.Replace(content);
+    }
+}
